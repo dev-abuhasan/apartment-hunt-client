974b4f4a727114f5d2d4a6a567f5c830e87ec076
ABU HASAN <ahinstitutte@gmail.com>
2020-11-15 22:51:07 +0600

    login error fix

--- src/Components/AuthData/Login.js
+++ src/Components/AuthData/Login.js
@@ -1,63 +1,132 @@
-import React, { useContext } from 'react';
-import './Css/Style.scss'
+import React, { useContext, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import './Css/Style.scss';
+import Formsy, { addValidationRule } from 'formsy-react';
+import MyInput from './MyInput';
 import { Link, useHistory, useLocation } from 'react-router-dom';
-import googleIcon from '../images/icons/google.png';
-import logo from '../images/logos/Logo.png';
+import google from '../images/icons/google.png';
 import { AuthContext } from './Auth';
-import { ExtraDataContext } from '../ExtraData/ExtraData';
+import Navbars from '../Pages/ShearCompo/Navbars';
+
+//form Validation check by warnings START
+const errors = {
+    isEmail: 'You have to type a valid email',
+    maxLength: 'You cannot type more than 25 characters',
+    minLength: 'You must type more than 6 characters',
+    isAlpha: 'You can only type letters',
+    equalsField: 'Password is not match'
+}
+//form Validation check by warnings END
 
 const Login = () => {
-    document.title = "Login-Pages";
+    const [canSubmit, setCanSubmit] = useState(false)
+    addValidationRule('isStrong', function (values, value) {
+        return ['$', '%'].some(v => value && value.indexOf(v) !== -1)
+    })
+    const disableButton = () => {
+        setCanSubmit(false)
+    }
+    const enableButton = () => {
+        setCanSubmit(true)
+    }
+
+    //Handle Firebase Auth
     //get user login form auth.js file
     const auth = useContext(AuthContext);
-    const { handleGoogleSignIn } = auth;
+    const { handleGoogleSignIn, facebookRegistration, loginInUserEmailAndPass, error } = auth;
 
     //redirect to path settings
     const location = useLocation();
     const history = useHistory()
-    let { from } = location.state || { from: { pathname: "/dashboard" } };
+    let { from } = location.state || { from: { pathname: "/" } };
 
     //google sign in 
     const handGoogleSign = () => {
-
         handleGoogleSignIn()
             .then(r => {
                 history.replace(from);
             })
     }
 
-    //ExtraData Context
-    const extraData = useContext(ExtraDataContext);
-    const {oldUser,setOldUser} = extraData;
-    console.log(oldUser);
+    //facebook sign in
+    const handFacebookSign = () => {
+        facebookRegistration()
+            .then(r => {
+                history.replace(from);
+            })
+    }
+    //login a user with email and pass
+    const submitLogin = (e) => {
+        if (e.email && e.password) {
+            loginInUserEmailAndPass(e.email, e.password)
+                .then(r => history.replace(from))
+        }
+        //e.preventDefault();
+    }
+    const changePath = () => {
+        const url = `/reset-password`;
+        history.push(url);
+    }
 
-    
     return (
-        <Container id="login" className="">
-            <div className="logo-img">
-                <Link to="/">
-                    <img src={logo} alt="logo..." />
-                </Link>
-            </div>
-            <div className="row">
-                <div className="signed-in-option col-md-6 m-auto">
-                    <div className="title">
+        <Container id="login">
+            <Navbars />
+            <br />
+            <br />
+            <section>
+                <div className="title-login">
+                    <h3>Login</h3>
+                </div>
+                <Formsy className='form' onValidSubmit={submitLogin} onValid={enableButton} onInvalid={disableButton}>
+                    <h2 className="text-dark">Login</h2>
 
-                        <h3>Login With</h3>
+                    <MyInput label="" type="text" name="email" validations="maxLength:25,isEmail" validationErrors={errors} placeholder="Email address..." required />
+
+                    <MyInput label="" type="password" name="password" validations="minLength:6" validationErrors={errors} placeholder="password..." required />
+
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div>
+                            <input type="checkbox" id="login-checkbox" /><span className="text-danger">Remember me</span>
+                        </div>
+                        <p className="text-dark cursor-pointer" onClick={() => changePath()}>Forgot your password</p>
                     </div>
-                    <div className="d-flex flex-column align-items-center">
+
+                    <br />
+                    <button type="submit" disabled={!canSubmit} className="">
+                        Log-In
+                </button>
+
+                    {error
+                        ? <p><span className="text-danger w-50">{error}</span></p>
+                        : <p></p>
+                    }
+                    <br />
+
+                    <p className="text-center text-dark">Created A Account?
+                    <Link className="Link" to='/sign-up'>
+                            Sign-Up
+                    </Link>
+                    </p>
+                </Formsy>
+                <div className="other-sign-option">
+                    <div style={{ color: "#fff" }}>
+                        ----------------------------------------or----------------------------------------
+                </div>
+                    <br />
+                    <div>
+                        <button className="login-sign-up-btn" onClick={() => handFacebookSign()}>
+                            <span className="float-left pl-3 google-icon"><img src={google} alt="" /></span>
+                            <span className="text-center">Continue with Google</span>
+                        </button>
                         <button className="login-sign-up-btn" onClick={() => handGoogleSign()}>
-                            <span className="float-left pl-3 google-icon"><img src={googleIcon} alt="" /></span>
-                            <span className="text-center mt-2 d-block">Continue with Google</span>
+                            <span className="float-left pl-3 google-icon"><img src={google} alt="" /></span>
+                            <span className="text-center">Continue with Google</span>
                         </button>
                     </div>
-                    <div className="title-footer text-center py-2">
-                        <p>Don't have account? <Link to="/" className="create-account">Create an account</Link> </p>
-                    </div>
                 </div>
-            </div>
+            </section>
         </Container>
     );
 };
+
 export default Login;
