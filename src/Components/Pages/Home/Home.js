2a1350b67ab3756387ad95f81d5ff43c3917f324
Md-Mahbub-Ahmed <67556397+Md-Mahbub-Ahmed@users.noreply.github.com>
2020-11-16 13:00:57 +0600

    Home page dane

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../ShearCompo/Footer';
 import Header from '../ShearCompo/Header';
+import HeaderBottom from '../ShearCompo/HeaderBottom';
 import Review from '../ShearCompo/Review';
 import Services from '../ShearCompo/Services';
 import './Home.scss';
@@ -8,14 +9,15 @@ import './Home.scss';
 const Home = () => {
     document.title = "Creative-Agency Home";
     return (
-        <>
+        <div style={{backgroundColor:"#F0F5F3"}}>
            <Header />
+           <HeaderBottom />
            <main id="main" className="container">
                 <Services />
                 <Review />
-                <Footer />
            </main>
-        </>
+           <Footer />
+        </div>
     );
 };
 
--- src/Components/Pages/ShearCompo/HeaderBottom.js
+++ src/Components/Pages/ShearCompo/HeaderBottom.js
@@ -6,20 +6,12 @@ import './Css/MediaQuery.scss'
 
 const HeaderBottom = () => {
     return (
-        <Row id="header-bottom" className="">
-            <Col lg={5}>
-                <div className="header-info p-5">
-                    <h3>Let's Grow Your Brand To The Next Level</h3>
-                    <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolore odio dolorum, voluptate autem </p>
-                    <button className="mt-3 btn btn-dark px-5 d-block">Hire us</button>
-                </div>
-            </Col>
-            <Col lg={7}>
-                <div className="header-sidebar-img p-5">
-                    <img src={headerSideImg} alt=""/>
-                </div>
-            </Col>
-        </Row>
+        
+        <div className="header-Background" style={{ textAlign: 'center', marginTop: '3%', marginBottom: '3%' }}>
+            <h1>FIND YOUR HOUSE RENT</h1><br />
+            <input ClassName="inputStyle" type="text" placeholder="Search..." />
+            <button ClassName="batStyle" type="button" class="btn btn-primary">Find Now</button>
+        </div>
     );
 };
 
