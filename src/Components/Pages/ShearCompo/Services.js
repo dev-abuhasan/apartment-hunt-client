0843b0b8d8eb623fdeafe71e1a2c7e28bdb2d022
Md-Mahbub-Ahmed <67556397+Md-Mahbub-Ahmed@users.noreply.github.com>
2020-11-16 13:02:12 +0600

    Home page done

--- src/Components/Pages/ShearCompo/Services.js
+++ src/Components/Pages/ShearCompo/Services.js
@@ -44,25 +44,25 @@ const Services = () => {
     return (
         <Row>
             <div className="services-title text-center mt-5 mb-3 col-sm-12">
-                <h2  className="card-color">Discover the latest Rent<br/>available today</h2>
+                <h2 className="card-color">Discover the latest Rent<br />available today</h2>
             </div>
             {serviceData.length > 0 ? serviceData.map(data =>
                 <Card className="card-Style" style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={data.img} />
-                <Card.Body>
-                <Card.Title className="card-color">{data.name}</Card.Title>
-                  <Card.Text>
-                  <img style={{width:'15px'}} src={LocationOnIcon} alt=""/> {data.location} 
-                  </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-            <ListGroupItem><img style={{width:'20px'}} src={bad} alt=""/> {data.bad} Bedrooms <span style={{float:'right'}}> <img style={{width:'20px'}} src={bath} alt=""/> {data.bathroom} Bathroom</span></ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-            <Card.Link className="price" href="#">${data.price}</Card.Link>
-            <Card.Link className="card-color" href="#"><Button className="card-btn" variant="success">Wiew Details</Button></Card.Link>
-                </Card.Body>
-              </Card>
+                    <Card.Img variant="top" src={data.img} />
+                    <Card.Body>
+                        <Card.Title className="card-color">{data.name}</Card.Title>
+                        <Card.Text>
+                            <img style={{ width: '15px' }} src={LocationOnIcon} alt="" /> {data.location}
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem><img style={{ width: '20px' }} src={bad} alt="" /> {data.bad} Bedrooms <span style={{ float: 'right' }}> <img style={{ width: '20px' }} src={bath} alt="" /> {data.bathroom} Bathroom</span></ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                        <Card.Link className="price" href="#">${data.price}</Card.Link>
+                        <Card.Link className="card-color" href="#"><Button className="card-btn" variant="success">Wiew Details</Button></Card.Link>
+                    </Card.Body>
+                </Card>
             ) : <div className="m-auto">
                     <LoadingSpinner />
                 </div>
